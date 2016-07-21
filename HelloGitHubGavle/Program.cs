305b2e4c034402e367dd87c30252c0ed6ace677f
scKeijser <tobias.keijser@studentconsulting.com>
2016-07-21 10:47:40 +0200

    Update Program.cs
    Changed welcometext

--- HelloGitHubGavle/Program.cs
+++ HelloGitHubGavle/Program.cs
@@ -10,7 +10,7 @@ namespace HelloGitHubGavle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub");
+            Console.WriteLine("Hello VisualStudio");
         }
     }
 }
